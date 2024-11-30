9df10d8f58add0360091b22d4800bb6a56bb454f
ArminNouri <nouriarmin24@gmail.com>
2024-11-30 12:17:35 +0000

    refactor put into header and main files

---
+++ dom-merge-conflict/tasks/buttons-and-counter/src/header.js
@@ -0,0 +1,9 @@
+function header() {
+  const header = document.createElement("header");
+  header.innerHTML = `
+      <h1>Number Counter</h1>
+      <p>A simple counter. Press increment to increase the count by one.</p>
+  `;
+  body.appendChild(header);
+}
+module.exports = header;
